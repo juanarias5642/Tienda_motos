384d5df42c860be23182942e369bddb498ffe9a7
ASUS <ASUS@PC-ME-OLPE>
2025-05-26 11:43:17 -0500

    cambio7

--- asp_presentacion/Program.cs
+++ asp_presentacion/Program.cs
@@ -2,9 +2,13 @@ using asp_presentacion;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1) Registra todos los servicios
 var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder, builder.Services);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
+
+// 2) Configura el pipeline HTTP
 startup.Configure(app, app.Environment);
+
 app.Run();
--- asp_presentacion/Startup.cs
+++ asp_presentacion/Startup.cs
@@ -1,20 +1,26 @@
 ﻿using lib_presentaciones.Implementaciones;
 using lib_presentaciones.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace asp_presentacion
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public static IConfiguration? Configuration { set; get; }
-
-        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        // Este método se llama por el runtime. Úsalo para agregar servicios al contenedor.
+        public void ConfigureServices(IServiceCollection services)
         {
-            // Presentaciones
+            // Inyección de dependencias para presentaciones
             services.AddScoped<IMarcasPresentacion, MarcasPresentacion>();
             services.AddScoped<ITiposPresentacion, TiposPresentacion>();
             services.AddScoped<IChasisesPresentacion, ChasisesPresentacion>();
@@ -24,8 +30,7 @@ namespace asp_presentacion
             services.AddScoped<IFacturasPresentacion, FacturasPresentacion>();
             services.AddScoped<IFact_motosPresentacion, Fact_motosPresentacion>();
 
-
-
+            // Añadir servicios de MVC, Razor Pages y sesiones
             services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddRazorPages();
@@ -35,18 +40,33 @@ namespace asp_presentacion
             });
         }
 
-        public void Configure(WebApplication app, IWebHostEnvironment env)
+        // Este método se llama por el runtime. Úsalo para configurar el pipeline HTTP.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (!app.Environment.IsDevelopment())
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
+
+            // Sirve archivos estáticos desde wwwroot
             app.UseStaticFiles();
+
             app.UseRouting();
-            app.UseAuthorization();
-            app.MapRazorPages();
+
             app.UseSession();
-            app.Run();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
         }
     }
 }
